3bbf3722faae0b9ef5144c51418d7f7a980e5278
Asduveneck <asduveneck@ucdavis.edu>
2019-11-25 23:06:53 -0800

    Starts up JS project

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: "./lib/map_viz.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "mainOutput.bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          query: {
+            presets: ["@babel/env"]
+          }
+        }
+      }
+    ]
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: [".js"]
+  }
+};
